2aa1bf209fc48197c0e413bb6d34470b154e33cc
bearcattt <ewigglet8@gmail.com>
2024-09-01 19:22:09 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lunar.github.io',
+    site: 'https://bearcattt.github.io/Lunar/',
  
 });
