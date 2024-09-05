f2d776d2e1a0cb01c32905164f0a0609495b8684
bearcattt <ewigglet8@gmail.com>
2024-09-04 21:22:32 -0400

    Optimizing & fixing

--- index.ts
+++ index.ts
@@ -9,47 +9,49 @@ import { promisify } from "node:util";
 import chalk from "chalk";
 
 const execPromise = promisify(exec);
-const port = "8080" as string;
+const port = process.env.PORT || "8080";  // You can change this to a port of your choosing
 const app = Fastify({ logger: false });
 
-if (!fs.existsSync("dist")) {
-  try {
+
+(async () => {
+  if (!fs.existsSync("dist")) {
     console.log(chalk.blue.bold("Dist not found, building..."));
-    await execPromise("npm run build");
-    console.log(chalk.green.bold("Dist successfully built!"));
-  } catch (e) {
-    console.log(chalk.red.bold("Unable to build dist folder", e));
+    try {
+      await execPromise("npm run build");
+      console.log(chalk.green.bold("Dist successfully built!"));
+    } catch (e) {
+      console.error(chalk.red.bold("Failed to build dist folder", e));
+      process.exit(1);
+    }
+  }
+
+
+  await app.register(fastifyMiddie);
+
+  const ssrfle = "./dist/server/entry.mjs";
+  if (fs.existsSync(ssrfle)) {
+    try {
+      const { handler: ssrHandler } = await import(ssrfle);
+      app.use(ssrHandler);
+      console.log(chalk.green.bold("Successfully registered handler."));
+    } catch (e) {
+      console.error(chalk.yellow.bold("Failed to import handler:", e));
+    }
+  } else {
+    console.error(chalk.red.bold("handler not found."));
     process.exit(1);
   }
-}
 
-await app.register(fastifyMiddie);
-
-let ssrHandler;
-if (fs.existsSync("./dist/server/entry.mjs")) {
-  try {
-    const module = await import("./dist/server/entry.mjs");
-    ssrHandler = module.handler;
-    app.use(ssrHandler);
-    console.log(chalk.green.bold("Successfully registered SSR handler."));
-  } catch (e) {
-    console.error(chalk.yellow.bold("Failed to import SSR handler:", e));
-  }
-} else {
-  console.log(chalk.red.bold("SSR handler not found."));
-  process.exit(1);
-}
-
-await app.register(fastifyStatic, {
-  root: fileURLToPath(new URL("./dist/client", import.meta.url)),
-});
+  await app.register(fastifyStatic, {
+    root: fileURLToPath(new URL("./dist/client", import.meta.url)),
+  });
 
 app.listen(port, (err, address) => {
   if (err) {
     console.log(chalk.blue.bold("The following error happend", err));
   } else {
     console.log(chalk.blue.bold("Lunar is running on:"));
-    console.log(chalk.blue.bold(`http://localhost:${port}`));
-    console.log(chalk.blue.bold(`${address}`));
+    console.log(chalk.blue.bold('http://localhost:${port}'));
+    console.log(chalk.blue.bold(${address}));
   }
 });
