405986f2073396db2f3ec41030f4cd10de9c197f
Bwlake <152343852+Bwlake@users.noreply.github.com>
2024-09-06 14:50:52 -0400

    Create main.ts

---
+++ src/utils/main.ts
@@ -0,0 +1,6 @@
+const fm = document.getElementById("sear") as HTMLFormElement;
+const input = document.getElementById("input") as HTMLFormElement;
+fm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+  localStorage.setItem("@lunar/gourl", input.value.trim())
+});
