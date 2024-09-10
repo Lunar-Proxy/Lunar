7c8f7f000f30947db3039fa76d01cfc05bdd00f0
Bearcat <pandaslifeyt@gmail.com>
2024-09-09 21:23:17 -0400

    fixing script srcs

--- src/utils/go.ts
+++ src/utils/go.ts
@@ -1,20 +1,17 @@
-import BareMuxConnection from "@mercuryworkshop/bare-mux";
-
-const connection = new BareMuxConnection("/b/worker.js");
-const wispUrl: string =
-  (location.protocol === "https:" ? "wss" : "ws") +
-  "://" +
-  location.host +
-  "/wisp/";
-
-if ((await connection.getTransport()) !== "/e/index.mjs") {
-  await connection.setTransport("/e/index.mjs", [{ wisp: wispUrl }]);
-}
 
 window.addEventListener('DOMContentLoaded', () => {
-  const iframe = document.createElement('iframe');
+  document.title =
+  localStorage.getItem("@lunar/cloak/title") || "Home - Google Drive";
+let link: HTMLLinkElement =
+  (document.querySelector("link[rel='icon']") as HTMLLinkElement) ||
+  document.createElement("link");
+link.rel = "icon";
+link.href =
+  localStorage.getItem("@lunar/cloak/favicon") || "./assets/favicon/drive.svg";
+document.head.appendChild(link);
+  const iframe = document.createElement('iframe') as HTMLIFrameElement;
   iframe.src = localStorage.getItem('@lunar/gourl') || 'https://google.com';
-  iframe.style.height = '100vh';
+  iframe.style.height = '100vh' as string;
   iframe.style.width = '100vw';
   document.body.appendChild(iframe);
 
