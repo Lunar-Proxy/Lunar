0984b288d9deaf75e51ef05e14a5f2918d3679e8
Bearcat <pandaslifeyt@gmail.com>
2024-09-11 22:01:06 -0400

    fixing astro config

--- astro.config.ts
+++ astro.config.ts
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "hybrid",
 
+  adapter: node({
+    mode: "middleware",
+  }),
+
   integrations: [tailwind()],
 });
