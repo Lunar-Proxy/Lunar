f4187c2d7a61bd3a827682f5208fd88e4798ac77
Bearcat <pandaslifeyt@gmail.com>
2024-09-12 17:32:55 -0400

    adding color & removing unessasary packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgcolr: "#303446",
+        link: "#89b4fa",
+        head: "#c6d0f5",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".color-link": {
+          color: "#89b4fa",
+        },
+        ".color-head": {
+          color: "#c6d0f5",
+        },
+      });
+    },
+  ],
 };
