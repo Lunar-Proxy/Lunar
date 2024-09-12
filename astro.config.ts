cda4282750f0fa4b5f7b759d2a4cd99591a424e5
Bearcat <pandaslifeyt@gmail.com>
2024-09-11 22:00:14 -0400

    removing vercel

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 export default defineConfig({
   output: "hybrid",
-
-  adapter: vercel(),
 
   integrations: [tailwind()],
 });
