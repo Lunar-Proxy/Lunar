ef15d68018e91d6e5b53b6b0f2695ad84cd3370d
Bearcat <pandaslifeyt@gmail.com>
2024-09-12 17:41:33 -0400

    formatting sw

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,15 @@
-
-importScripts('./assets/u/bundle.js');
-importScripts('./assets/u/config.js');
-importScripts(config.sw || './assets/u/sw.js');
+importScripts("./assets/u/bundle.js");
+importScripts("./assets/u/config.js");
+importScripts(config.sw || "./assets/u/sw.js");
 const uv = new UVServiceWorker();
 async function handleRequest(event) {
-    if (uv.route(event)) {
-        return await uv.fetch(event);
-    }
-    
-    return await fetch(event.request)
+  if (uv.route(event)) {
+    return await uv.fetch(event);
+  }
+
+  return await fetch(event.request);
 }
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(handleRequest(event));
+self.addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event));
 });
