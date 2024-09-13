d4d05a8b456af89c27c6adea7da76f34fd756ded
Bearcat <pandaslifeyt@gmail.com>
2024-09-13 16:38:57 -0400

    formatted, fixing styles, and optimizations

--- public/assets/u/config.js
+++ public/assets/u/config.js
@@ -2,23 +2,23 @@ const config = {
   prefix: "/p/",
   encodeUrl: (str) => {
     if (!str) return str;
-    let result = "";
+    const result = new Array(str.length);
     for (let i = 0; i < str.length; i++) {
-      result += i % 2 ? String.fromCharCode(str.charCodeAt(i) ^ 2) : str[i];
+      result[i] = i % 2 ? String.fromCharCode(str.charCodeAt(i) ^ 2) : str[i];
     }
-    return encodeURIComponent(result);
+    return encodeURIComponent(result.join(''));
   },
+  
   decodeUrl: (str) => {
     if (!str) return str;
     const [input, ...search] = str.split("?");
-    let result = "";
     const decoded = decodeURIComponent(input);
+    const result = new Array(decoded.length);
     for (let i = 0; i < decoded.length; i++) {
-      result +=
-        i % 2 ? String.fromCharCode(decoded.charCodeAt(i) ^ 2) : decoded[i];
+      result[i] = i % 2 ? String.fromCharCode(decoded.charCodeAt(i) ^ 2) : decoded[i];
     }
-    return result + (search.length ? "?" + search.join("?") : "");
-  },
+    return result.join('') + (search.length ? "?" + search.join("?") : "");
+  },  
   handler: "./assets/u/handler.js",
   client: "./assets/u/client.js",
   bundle: "./assets/u/bundle.js",
--- src/utils/go.ts
+++ src/utils/go.ts
@@ -1,30 +1,33 @@
+const iframe = document.createElement("iframe");
 window.addEventListener("DOMContentLoaded", () => {
-  document.title =
+  const titl =
     localStorage.getItem("@lunar/cloak/title") || "Home - Google Drive";
+  const favi =
+    localStorage.getItem("@lunar/cloak/favicon") ||
+    "./assets/favicon/drive.svg";
+  const gourl =
+    localStorage.getItem("@lunar/gourl") || "/p/hvtrs8%2F-Gmoelg.aoo";
+
+  document.title = titl;
+
   let link: HTMLLinkElement =
     (document.querySelector("link[rel='icon']") as HTMLLinkElement) ||
     document.createElement("link");
   link.rel = "icon";
-  link.href =
-    localStorage.getItem("@lunar/cloak/favicon") ||
-    "./assets/favicon/drive.svg";
+  link.href = favi;
   document.head.appendChild(link);
-  const iframe = document.createElement("iframe") as HTMLIFrameElement;
-  try {
-    iframe.src =
-      localStorage.getItem("@lunar/gourl") || "/p/hvtrs8%2F-Gmoelg.aoo";
-  } catch (e) {
-    console.error("unable to load your content, please try again later", e);
-    alert("An error occured trying to load your content" + e);
-  }
-  iframe.style.height = "100vh";
-  iframe.style.width = "100vw";
-  document.body.appendChild(iframe);
 
-  iframe.addEventListener("load", () => {
-    const loadingDiv = document.getElementById("loading");
-    if (loadingDiv) {
-      loadingDiv.style.display = "none";
-    }
-  });
+  if (iframe) {
+    iframe.src = gourl;
+    iframe.style.height = "100vh";
+    iframe.style.width = "100vw";
+    document.body.appendChild(iframe);
+
+    iframe.addEventListener("load", () => {
+      const loadingDiv = document.getElementById("loading");
+      if (loadingDiv) {
+        loadingDiv.style.display = "none";
+      }
+    });
+  }
 });
