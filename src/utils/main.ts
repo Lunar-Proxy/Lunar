4f26681943acb67730201ee87edc7776b424de3f
bearcatt <bearcat.example.com>
2024-09-16 00:23:40 -0400

    updating css & better url  finder code & formatted

--- src/utils/main.ts
+++ src/utils/main.ts
@@ -1,33 +1,20 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./sw.js", { scope: "/p/" })
-      .then(({ scope }) =>
-        console.log(
-          "Successfully registered Service Workers with scope:",
-          scope,
-        ),
-      )
-      .catch((error) =>
-        console.error("Failed to register Service Worker:", error),
-      );
-  });
-}
-
-const fm = document.getElementById("sear") as HTMLFormElement;
 const input = document.getElementById("input") as HTMLInputElement;
-const urlPattern =
-  /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/.*)?$/;
 
-fm.addEventListener("submit", (event) => {
+document.getElementById("sear")?.addEventListener("submit", (event) => {
   event.preventDefault();
-  const value = input.value.trim();
 
-  let url;
-  if (urlPattern.test(value)) {
+  const value = input.value.trim();
+  const regex = /^(https?:\/\/)/;
+
+  let url: string;
+
+  if (regex.test(value)) {
     url = value;
   } else {
-    url = `https://www.google.com/search?q=${encodeURIComponent(value)}`;
+    url = `https://${value}`;
+    if (!/^https?:\/\/.+$/.test(url)) {
+      url = `https://www.google.com/search?q=${encodeURIComponent(value)}`;
+    }
   }
   // @ts-ignore
   localStorage.setItem("@lunar/gourl", `/p/${config.encodeUrl(url)}`);
